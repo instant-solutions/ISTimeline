218ab2c4b9ad35d0e1e50576b1db74e6c006b12c
Max Holzleitner <max.holzleitner@instant-it.at>
2016-05-07 20:38:37 +0200

    initial commit

---
+++ ISTimeline/ISTimeline/ISTimeline.h
@@ -0,0 +1,19 @@
+//
+//  ISTimeline.h
+//  ISTimeline
+//
+//  Created by Max Holzleitner on 07.05.16.
+//  Copyright © 2016 instant:solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ISTimeline.
+FOUNDATION_EXPORT double ISTimelineVersionNumber;
+
+//! Project version string for ISTimeline.
+FOUNDATION_EXPORT const unsigned char ISTimelineVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ISTimeline/PublicHeader.h>
+
+
