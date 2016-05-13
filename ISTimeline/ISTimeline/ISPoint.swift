a20fa1b975cb2300ad38435e1a447fd8c23fef10
Max Holzleitner <max.holzleitner@instant-it.at>
2016-05-14 00:52:24 +0200

    added a class to handle the time points

---
+++ ISTimeline/ISTimeline/ISPoint.swift
@@ -0,0 +1,20 @@
+//
+//  ISPoint.swift
+//  ISTimeline
+//
+//  Created by Max Holzleitner on 13.05.16.
+//  Copyright © 2016 instant:solutions. All rights reserved.
+//
+
+class ISPoint {
+    
+    var title:String = ""
+    var description:String = ""
+    var touchUpInside:Optional<(point:ISPoint) -> Void>
+    
+    init(title:String, description:String, touchUpInside:Optional<(point:ISPoint) -> Void>) {
+        self.title = title
+        self.description = description
+        self.touchUpInside = touchUpInside
+    }
+}
