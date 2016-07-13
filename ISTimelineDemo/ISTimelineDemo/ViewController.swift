500b387a5b5f72109c064806d50476f4f9589001
Max Holzleitner <max.holzleitner@instant-it.at>
2016-07-13 14:23:57 +0200

    initial demo app commit

---
+++ ISTimelineDemo/ISTimelineDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ISTimelineDemo
+//
+//  Created by Max Holzleitner on 13.07.16.
+//  Copyright © 2016 instant:solutions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
